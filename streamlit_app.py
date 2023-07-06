88f17322bfceec5faecdfbe073d13ae866e5a9e8
Igor Krupkin <krupkin.ig@mail.ru>
2023-07-06 21:28:36 +0500

    Ðfirst update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
-import streamlit
+import streamlit as st
 
+x = st.slider("Выберите свой возраст", min_value=18, max_value=60)
+
+if x < 30:
+    st.write("Какой вы молодой!")
+if 30 <= x < 50:
+    st.write("В расцвете сил!")
+if x >= 50:
+    st.write("Старичок, пора в гробик на бочок!")
